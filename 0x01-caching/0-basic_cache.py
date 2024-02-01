9e60db92e94bf2bc2785749037d86c3ab6538e90
Hassan Cissey <hcissey0@gmail.com>
2024-01-31 16:54:16 -0800

    Basic Caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""This is the basic caching file"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """This is the Basic Cache class"""
+
+    def put(self, key, item):
+        """This is the put function to put something to the cache"""
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """returns the value of the key cache"""
+        return self.cache_data.get(key)
