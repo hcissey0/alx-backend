27385e11c891513ae51ff3e8513ba26508959f07
Hassan Cissey <hcissey0@gmail.com>
2024-02-07 04:54:40 +0000

    force local

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""This is the flask app file"""
+
+from flask import (
+    Flask,
+    render_template,
+    request,
+    g,
+)
+from flask_babel import Babel
+from typing import (
+    Text,
+    Optional
+)
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+class Config:
+    """This is the config class for the flask app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> Optional[str]:
+    """The function that gets the locale
+
+    Returns:
+        Optional[str]: The locale
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def home() -> Text:
+    """This is the home route
+
+    Returns:
+        Text: The html template file
+    """
+    return render_template("4-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
