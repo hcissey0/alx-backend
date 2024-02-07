84cce9bb299854fad133cc4590aed00b9ddf2159
Hassan Cissey <hcissey0@gmail.com>
2024-02-07 05:01:47 +0000

    force local

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -39,7 +39,7 @@ def get_locale() -> Optional[str]:
     """
     if 'locale'in request.args:
         locale = request.args.get('locale')
-        if locale in app.config["SUPPORTED_LOCALES"]:
+        if locale in app.config["LANGUAGES"]:
             return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
