723b394f061912dbae9ddcc64951e43ea691111b
Hassan Cissey <hcissey0@gmail.com>
2024-03-07 05:37:26 +0000

    redis

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -3,11 +3,9 @@
 // Import the necessary library (using ES6 import syntax)
 import redis from 'redis';
 
+
 // Create a Redis client
-const client = redis.createClient({
-  host: '127.0.0.1', // Redis server address (localhost)
-  port: 6379, // Redis server port
-});
+const client = redis.createClient();
 
 // Attempt to connect to the Redis server
 client.on('connect', () => {
@@ -19,14 +17,3 @@ client.on('error', (err) => {
   console.error(`Redis client not connected to the server: ${err.message}`);
 });
 
-// Close the Redis connection when done (optional)
-// client.quit();
-
-// Example usage: Set a key-value pair
-client.set('myKey', 'myValue', (err, reply) => {
-  if (err) {
-    console.error(`Error setting key: ${err.message}`);
-  } else {
-    console.log(`Key set successfully: ${reply}`);
-  }
-});
